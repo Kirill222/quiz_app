48bff2f0805df0deade84f9ab4a89d7307a1bcd6
John Connor <zkmd2012@inbox.ru>
2021-10-02 19:00:35 +0500

    01. Statr of the project. Simple components are created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useState} from 'react'
+import MainMenu from './#components/MainMenu';
+import Quiz from './#components/Quiz';
+import EndScreen from './#components/EndScreen';
 
 function App() {
+
+  const [gameState, setGameState] = useState('menu')
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi</h1>
+
+      {gameState === 'menu' && <MainMenu />}
+      {gameState === 'quiz' && <Quiz />}
+      {gameState === 'endScreen' && <EndScreen />}
+
     </div>
   );
 }
